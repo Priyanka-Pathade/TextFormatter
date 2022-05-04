2aae348b15a32eeb8e658e669cfc4d5a3d328a20
PRIYANKA PATHADE <priyankapathade4773@gmail.com>
2022-05-04 12:44:36 +0530

    word count has fixed

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -46,7 +46,7 @@ export default function TextForm(props) {
         setText(res1);
         props.showAlert("Removed all symbols except '/'","success");
         };
-        const handleTitleCase = () => {
+    const handleTitleCase = () => {
             let newText = text.split(" ").map((currentValue) => {
                 let newText = currentValue[0].toUpperCase() + currentValue.slice(1);
                 return newText;
@@ -54,7 +54,21 @@ export default function TextForm(props) {
             setText(newText.join(" "));
             props.showAlert("Converted to title case","success");
         }
-   
+    function wordCount(content){
+            if(content===""){
+              return 0;
+            }
+            let arr = content.split(" ");
+            let len = arr.length;
+            let count=0;
+            
+            for(let i=0;i<len;i++){
+              if(arr[i]==='' || arr[i]===' '){
+                 count++;
+              }
+            }
+            return len-count;
+          }
     const [text,setText] = useState('');
   //  setText=('New Text');
   return (
@@ -65,19 +79,20 @@ export default function TextForm(props) {
         <label htmlFor="myBox" className="form-label" >Example text area</label>
         <textarea className="form-control" id="myBox" style={{backgroundColor:props.mode==='light'?'white':'grey', color:props.mode==='light'?'black':'white'}} name="myText" value={text} onChange={handleOnChange} rows="3"></textarea>
     </div>
-    <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-    <button className="btn btn-primary mx-1" onClick={handleLowClick}>Convert to LowerCase</button>
-    <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra spaces</button>
-    <button className="btn btn-primary mx-1" onClick={handleTextExtract}>Remove symbols</button>
-    <button className="btn btn-primary mx-1" onClick={handleTitleCase}>Title Case</button>
-    <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
-    <button className="btn btn-primary mx-1" onClick={handleSpeak}>Speak</button>
-    <button className="btn btn-primary mx-1" onClick={clearText}>Clear</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleLowClick}>Convert to LowerCase</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleExtraSpaces}>Remove Extra spaces</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleTextExtract}>Remove symbols</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleTitleCase}>Title Case</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleCopy}>Copy Text</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={handleSpeak}>Speak</button>
+    <button className="btn btn-primary mx-1 my-1" onClick={clearText}>Clear</button>
 
     </div>
     <div className="container my-3" style={{color:props.mode==='light'?'black':'white'}}>
         <h2>Your text summary</h2>
-        <p>{text.split(" ").length} words and {text.length} characters</p>
+        {/* <p>{text.split(" ").length} words and {text.length} characters</p> */}
+        <p>{wordCount(text)} words and {text.length} characters</p>
         <p>{0.008 * text.split(" ").length} Minutes to read </p>
         <h2>Preview</h2>
         <p>{text.length>0?text:'Enter something in the textbox above to preview it here'}</p>
